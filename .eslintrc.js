ce586345be6e2a0f01f6ead67d94ca7f1264c4d8
terukazu inoue <terukazu.inoue@codianz.com>
2020-08-05 16:34:54 +0900

    eslintの設定変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,10 +21,6 @@ module.exports = {
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/class-name-casing': 'off',
     '@typescript-eslint/no-inferrable-types': 'off',
-    '@typescript-eslint/adjacent-overload-signatures': 'off',
-
-    /* KANDEN-EV-SIM special */
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': 'off'
+    '@typescript-eslint/adjacent-overload-signatures': 'off'
   }
 }
